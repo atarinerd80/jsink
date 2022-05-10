8e72766c19983a0602496cdb7f3bd7d02d0330f9
Thomas Slagle <tom.slagle@gmail.com>
2022-05-10 12:46:10 -0400

    Updated about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,38 +12,15 @@ export class About extends React.Component {
                                 <div className="border"></div>
                             </div>
                             <div className="col-lg-6 padding-0 ">
-                                <img className="img-responsive" src="images/about/about-business-man.jpg" alt="" />
+                                <img className="img-responsive" src="images/about/about.jpg" alt="" />
                             </div>
                             <div className="col-lg-6">
                                 <div className="content-block">
-                                    <h2>We’re A Digital Design Agency.</h2>
+                                    <h2>Monticello's Only Tattoo Shop</h2>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam tempora itaque, autem dolores culpa cum mollitia voluptate nesciunt voluptatibus quasi.</p>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eveniet vel et mollitia nemo corporis sed ut, exercitationem incidunt, rerum nam doloremque quos ratione doloribus, officiis adipisci error quasi soluta?</p>
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, magnam.</p>
-                                    <div className="row">
-                                        <div className="col-lg-6">
-                                            <div className="media">
-                                                <div className="pull-left">
-                                                <i className="tf-circle-compass"></i>	
-                                                </div>
-                                                <div className="media-body">
-                                                <h4 className="media-heading">SEO Optimized</h4>
-                                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus consectetur dolores repellendus eos saepe</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-6">
-                                            <div className="media">
-                                                <div className="pull-left">
-                                                <i className="tf-hotairballoon"></i>	
-                                                </div>
-                                                <div className="media-body">
-                                                <h4 className="media-heading">Easy Customization</h4>
-                                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus consectetur dolores repellendus </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eveniet vel et mollitia nemo corporis sed ut, exercitationem incidunt, rerum nam doloremque quos ratione doloribus, officiis adipisci error quasi soluta?</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eveniet vel et mollitia nemo corporis sed ut, exercitationem incidunt, rerum nam doloremque quos ratione doloribus, officiis adipisci error quasi soluta?</p>
                                 </div>
                             </div>
                         </div>
