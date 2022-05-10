a7c5f981d3ee2893049ee44755c9bf26da082ed2
Thomas Slagle <tom.slagle@gmail.com>
2022-05-10 15:59:14 -0400

    Rearranged contact information

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,10 +14,14 @@ export class Contact extends React.Component {
                             </div>
                             <div className="contact-info col-lg-6 wow fadeInUp center">
                                 <h3>Contact Details</h3>
-                                <span>945 N Main St, Monticello, KY 42633</span><br />
-                                <span>Phone: <a href="teL:6063076327">(606) 307-6327</a></span><br />
-                                <span>Facebook: <a href="https://www.facebook.com/jason.slagle.967" target="_blank">Jason Slagle</a></span><br />
-                                <span>Instagram: <a href="https://www.instagram.com/jsink_/?hl=en" target="_blank">JS Ink</a></span>
+                                <h6>Address</h6>
+                                <span>945 N Main St, Monticello, KY 42633</span><br /><br />
+                                <h6>Phone Number</h6>
+                                <span><a href="teL:6063076327">(606) 307-6327</a></span><br /><br />
+                                <h6>Facebook</h6>
+                                <span><a href="https://www.facebook.com/jason.slagle.967" target="_blank">Jason Slagle</a></span><br /><br />
+                                <h6>Instagram</h6>
+                                <span><a href="https://www.instagram.com/jsink_/?hl=en" target="_blank">JS Ink</a></span>
                             </div>
                             <div className="col-lg-6 map-container">
                                 <Map />
